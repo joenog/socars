ac3f592d02e6566fabfe87f9edeabfcc337b0631
joenog <joabnogueira@outlook.com>
2025-05-09 18:35:41 -0300

    login created

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,14 @@
+import { Container } from "../../components/container"
+
 export default function Dashboard() {
     return(
       <>
-      <h1>DashBoard conta logada</h1>
+        <Container>
+          <h1>DashBoard conta logada</h1>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia repellat error corporis, voluptatibus, maxime, vero sed culpa dilore Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, temporibus neque? Numquam velit, animi eaque illo sed nobis, unde pariatur eos, corporis accusantium veritatis excepturi! Vitae assumenda sint et totam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, amet cupiditate soluta quod qui eligendi quidem animi fuga magni nam vel possimus. A illo aspernatur reiciendis repellat quaerat ducimus? Repellat.cta iste eveniet assumenda nesciunt temporibus impedit repudiandae recusandae alias? Itaque, laborum dolores!
+          </div>
+        </Container>
       </>
     )
   }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,66 @@
+import { Container } from "../../components/container";
+import logoImg from "../../assets/socars-logo.png";
+import { Link } from "react-router-dom";
+import Input from "../../components/input";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
 export default function Login() {
+
+  const schema = z.object({
+    email: z.string().email("Insira um email válido!").nonempty("O campo email é obrigatório!"),
+    password: z.string().nonempty("O campo senha é obrigatório!")
+  })
+
+  type FormData = z.infer<typeof schema>
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange"
+  })
+
+  function onSubmit(data: FormData) {
+    console.log(data)
+  }
+
     return(
-      <>
-      <h1>Login</h1>
-      </>
+      <Container>
+        <div className="flex flex-col w-full justify-center items-center gap-4">
+          <Link className="items-center mb-2" to={"/"}>
+            <img src={logoImg} alt="logo-site" />
+          </Link>
+
+          <form className="w-full md:max-w-xl my-8 p-4 bg-white rounded-md" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-2">
+              <Input
+               type="email"
+               placeholder="Digite seu email"
+               name="email"
+               error={errors.email?.message}
+               register={register}
+              />
+            </div>
+
+            <div className="mb-2">
+              <Input
+               type="password"
+               placeholder="Digite sua senha"
+               name="password"
+               error={errors.password?.message}
+               register={register}
+              />
+            </div>
+
+
+            <button className="w-full p-2 bg-gray-800 rounded-md text-white">
+              Acessar
+            </button>
+
+          </form>
+        </div>
+      </Container>
     )
   }
